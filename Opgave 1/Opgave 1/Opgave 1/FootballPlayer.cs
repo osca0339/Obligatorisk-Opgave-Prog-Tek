4c54b5ffef90a0bef1db999e11136ced8ad1eed6
Oscar Arklint Hemmingsen <osca5515@edu.easj.dk>
2021-09-16 10:34:27 +0200

    Made Football Player library class

---
+++ Opgave 1/Opgave 1/Opgave 1/FootballPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Opgave_1
+{
+    public class FootballPlayer
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+        private int _shirtNumber;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length <= 4)
+                {
+                    throw new ArgumentException("Name must be longer than or equal to 4 ");
+                }
+
+                _name = value;
+            }
+
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value < 1 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                _shirtNumber = value;
+            }
+        }
+    }
+}
